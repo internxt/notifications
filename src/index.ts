a25153c499c3362e45ef35adfdc77ee6baafa23a
Alex Menor <alex.menor@icloud.com>
2022-02-15 12:43:13 +0100

    add server initialisation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello there!!');
+import 'dotenv/config';
+
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+if (process.env.NODE_ENV === 'production') {
+  const pubClient = createClient({ url: process.env.REDIS_HOST, password: process.env.REDIS_PASSWORD });
+  const subClient = pubClient.duplicate();
+  io.adapter(createAdapter(pubClient, subClient));
+}
+
+const { PORT } = process.env;
+
+httpServer.listen(PORT, () => console.log(`Running on port ${PORT}`));
